37324746c8c9fea6b19048a94ea643dd9e370f82
kefilwe99 <wendykefilwe56@gmail.com>
2024-07-10 11:01:48 +0200

    stashing database connection and jdbc template getters and setters

--- src/main/java/com/CrudPracTodo/GroceryList/Repository/GroceryListRepository.java
+++ src/main/java/com/CrudPracTodo/GroceryList/Repository/GroceryListRepository.java
@@ -2,12 +2,25 @@ package com.CrudPracTodo.GroceryList.Repository;
 
 import com.CrudPracTodo.GroceryList.Model.groceryModel;
 import com.CrudPracTodo.GroceryList.Type.groceryInterface;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public class GroceryListRepository implements groceryInterface {
+
+   private JdbcTemplate jdbc;
+
+    public JdbcTemplate getJdbc() {
+        return jdbc;
+    }
+
+    @Autowired
+    public void setJdbc(JdbcTemplate jdbc) {
+        this.jdbc = jdbc;
+    }
 
     @Override
     public List<groceryModel> onRead() {
